932829522b57fcf5db4e4bbf093eca4aedaabf53
huubexel <huubexel@hotmail.com>
2023-10-03 16:18:51 +0200

    Added comments to next

--- week4/ass28/line/functions/next/next.cc
+++ week4/ass28/line/functions/next/next.cc
@@ -1,18 +1,12 @@
 #include "../../internal_header/line.ih"
 
-// a member std::string next() returning the next substring from the line retrieved by getLine()
-// consisting of non-ws characters, and an empty string if no such substring is available anymore.
-
-// make sure that at every call of next() the begin position of the next substring is already known, 
-//and that his begin position is equal to string::npos if there are no more substrings.
-
 string Line::next()
-{
+{                                 // If the index is string::npos, return ""
     if (d_index == string::npos)
-        return "";
-    
+        return "";                // Set the beginposition, if the first word
+                                  // is found, add one to omit the whitespace
     size_t beginPosition = (d_index != 0) ? d_index + 1 : d_index;
     d_index = d_line.find_first_of(" \t\n\v\f\r", beginPosition);
-
+                                  // Set the position of the next whitespace
     return d_line.substr(beginPosition, d_index - beginPosition);
-}
+}                                 // Return the substring
