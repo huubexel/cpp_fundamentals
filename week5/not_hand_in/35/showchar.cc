bf789db3be8822eb15da0f7afa010cc3c815da31
huubexel <huubexel@hotmail.com>
2023-10-08 11:38:39 +0200

    Added comments to showChar

--- week5/not_hand_in/35/showchar.cc
+++ week5/not_hand_in/35/showchar.cc
@@ -2,16 +2,16 @@
 
 void showChar(char const &ch)
 {
-    if (!isprint(ch))
+    if (!isprint(ch))                 // Check if character isn't printable
     {
-        if (ch == 9)
+        if (ch == 9)                  // if tab, print it with '\t'
             cout << "char '\\t'\n";
-        else if (ch == 10)
+        else if (ch == 10)            // if newline, print it with '\n'
             cout << "char '\\n'\n";
-        else
+        else                          // if not printable, print ASCII number
             cout << "char " << static_cast<int>(ch) << "\n";
         return;
     }
 
-    cout << "char '" << ch << "'\n";
-}
+    cout << "char '" << ch << "'\n";  // If char is printable, print
+}                                     // with single quotes around it
