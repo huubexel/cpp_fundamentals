b2913d8c5d9751bd856ab2a4945e7ac72c6b5df7
huubexel <huubexel@hotmail.com>
2023-10-16 13:01:40 +0200

    Deleted the old handed in things for 35

--- week5/hand_in/35/info/info.cc
+++
@@ -1,6 +0,0 @@
-#include "../internal_header/charcount.ih"
-
-CharInfo const &CharCount::info() const
-{
-    return d_charInfoStructObject; // Returns the data member of type
-}                                  // CharInfo (which is a struct)
