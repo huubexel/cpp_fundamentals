b426de6428cafdee14e045adf8f80ef4c8f75b25
huubexel <huubexel@hotmail.com>
2023-10-18 14:38:10 +0200

    Added raw memory info

--- test/chapter9/raw_memory/raw_memory.cc
+++ test/chapter9/raw_memory/raw_memory.cc
@@ -1,11 +1,27 @@
 #include "../main.ih"
 
+//'new version' named: raw memory
+// Syntax            : operator new OR operator new[];
+
+// The word, 'operator' is part of the syntax here.
+
 void rawMemory()
 {
+                                // room for 5 ints:
+    int *ip = static_cast<int *>(operator new(5 * sizeof(int)));
+                                // same as the previous example:
+    int *ip2 = static_cast<int *>(operator new[](5 * sizeof(int)));
+                                // room for 5 strings:
+    string *sp = static_cast<string *>(operator new(5 * sizeof(string)));
 
+    // These is raw memory, because raw memory returns void *, a static cast 
+    // needs to be done. These raw memory parts should not be seen as an 
+    // array, but should be seen as a series of memory locations that 
+    // dynamically have been made available. No initialization has been done
+    // by this kind of new.
+
+    // This is the delete syntax for this type of new.
+    operator delete(ip);
+    operator delete[](ip2);
+    operator delete(sp);
 }
-
-
-//Although raw memory may consist of multiple characters it should not be interpreted as an array of characters. 
-//Since raw memory returned by new is returned as a void * its return value can be assigned to a void * variable. 
-//More often it is assigned to a char * variable, using a cast. Here is an example:
